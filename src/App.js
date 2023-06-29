fbf0a9e1c6f7b96837fb6710f58436e70385a734
sonushetty26 <krishna8310800362@gmail.com>
2023-06-29 10:34:25 +0530

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Ex1 from './component/Ex1'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(props){
+  return(
+    <div>
+      <h1>States in React</h1>
+      <hr/>
+      <Ex1/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/component/Ex1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Ex1(props){
+    return(
+        <div>
+            <h3>States in Functional Component</h3>
+        </div>
+    )
+}
+
+export default Ex1
